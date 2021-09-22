ec1ee594faf2f9ef811a885c894c743a283a8bcd
Your Name <nataliia.kulieshova@gmail.com>
2021-09-21 20:44:31 -0400

    YES DEAR LORD FINALLY

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".header_navbar").removeClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo-svg.png");
+            $(".header_navbar img").attr("src", "assets/images/logo-2-svg.png");
         } else {
             $(".header_navbar").addClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo-2-svg.png");
+            $(".header_navbar img").attr("src", "assets/images/logo-svg.png");
         }
     });
     
